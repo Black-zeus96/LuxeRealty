76413384644e9689f104a85fec88d57f49e3cccb
Black-zeus96 <davidpeter1996.pd@gmail.com>
2024-07-13 03:29:30 -0400

    created update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,5 +1,6 @@
 import Listing from "../models/listing.model.js";
 
+// Create a listing
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
@@ -9,6 +10,7 @@ export const createListing = async (req, res, next) => {
   }
 };
 
+// Delete a listing
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
 
@@ -23,6 +25,28 @@ export const deleteListing = async (req, res, next) => {
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json('Listing has been deleted!');
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Update a listing
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'You can only update your own listings!'));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createListing, deleteListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, updateListing } from "../controllers/listing.controller.js";
 import { verifyUserToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUserToken, createListing);
 router.delete("/delete/:id", verifyUserToken, deleteListing);
-
+router.post('/update/:id', verifyUserToken, updateListing);
 export default router;
